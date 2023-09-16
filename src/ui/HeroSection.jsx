f6598ee11d17d7e5bc4dbdbe4a81b939fa4f7e02
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-15 20:04:26 -0600

    worked on the gero section

--- src/ui/HeroSection.jsx
+++ src/ui/HeroSection.jsx
@@ -1,7 +1,7 @@
 import rightArrow from "../assets/arrow-right.svg";
 function HeroSection() {
   return (
-    <div className="flex flex-col items-center pb-[134px] pl-[172px] pr-[152px] pt-[106px]">
+    <div className="flex flex-col items-center pb-[134px] pl-[150px] pr-[150px] pt-[106px]">
       <h1 className="text-primaryDark mb-10 text-center text-[78px] font-semibold">
         Empowering Africa&apos;s Future Unlocking Bitcoin&apos;s Potential, One
         Course at a Time
