449d6d59f28582b991a04618a37650703d892e36
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-15 19:59:55 -0600

    worked on the gero section

--- src/ui/HeroSection.jsx
+++ src/ui/HeroSection.jsx
@@ -1,12 +1,12 @@
 import rightArrow from "../assets/arrow-right.svg";
 function HeroSection() {
   return (
-    <div className="pb-[134px] pl-[172px] pr-[152px] pt-[106px]">
+    <div className="flex flex-col items-center pb-[134px] pl-[172px] pr-[152px] pt-[106px]">
       <h1 className="text-primaryDark mb-10 text-center text-[78px] font-semibold">
         Empowering Africa&apos;s Future Unlocking Bitcoin&apos;s Potential, One
         Course at a Time
       </h1>
-      <button className="border-primaryColor flex justify-center gap-x-10 rounded-[50px] border-[1.5px] border-solid py-5">
+      <button className="border-primaryColor text-secondaryDark inline-block flex justify-center gap-x-10 rounded-[50px] border-[1.5px] border-solid py-5 text-base font-[500]">
         Get started <img src={rightArrow} alt="" />
       </button>
     </div>
